9af2a21af6991e67bdc1de5b89aed26af2a8496e
matiaszm <48072636+matiaszm@users.noreply.github.com>
2022-04-25 09:02:51 -0400

    update views.py from book code

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,34 +1,30 @@
+# Create your views here.
 from rest_framework import generics, permissions
 from .serializers import TodoSerializer, TodoToggleCompleteSerializer
 from todo.models import Todo
 from django.db import IntegrityError
 from django.contrib.auth.models import User
-from rest_framework.parsers import JSONParser 
-from rest_framework.authtoken.models import Token 
+from rest_framework.parsers import JSONParser
+from rest_framework.authtoken.models import Token
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import authenticate
-# class TodoList(generics.ListAPIView):
-#    #ListAPIView necesita dos atributos obligatorios
-#    # una clase serializer y queryset
-#     serializer_class = TodoSerializer
-#     permission_classes = [permissions.IsAuthenticated]
 
-#     def get_queryset(self):
-#         user = self.request.user
-#         return Todo.objects.filter(user=user).order_by('-created')
+class TodoListCreate(generics.ListCreateAPIView): 
 
-class TodoListCreate(generics.ListCreateAPIView):
+    # The serializer class that should be used for validating and 
+    # deserializing input, and for serializing output
+    # We specify TodoSerializer which we have earlier implemented
     serializer_class = TodoSerializer
+    # permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
-        user = self.request.user
+        user = self.request.user        
         return Todo.objects.filter(user=user).order_by('-created')
 
     def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
         #serializer holds a django model
-        serializer.save(user=self.request.user)
+        serializer.save(user=self.request.user) 
 
 class TodoRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = TodoSerializer
@@ -36,7 +32,7 @@ class TodoRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
 
     def get_queryset(self):
         user = self.request.user
-        #user can only update,delete own posts
+        # user can only update, delete own posts
         return Todo.objects.filter(user=user)
 
 class TodoToggleComplete(generics.UpdateAPIView):
@@ -47,38 +43,41 @@ class TodoToggleComplete(generics.UpdateAPIView):
         user = self.request.user
         return Todo.objects.filter(user=user)
 
-    def perform_update(self, serializer):
-        serializer.instance.completed = not(serializer.instance.completed)
+    def perform_update(self,serializer):
+        serializer.instance.completed=not(serializer.instance.completed)
         serializer.save()
 
 @csrf_exempt
 def signup(request):
     if request.method == 'POST':
         try:
-            data = JSONParser().parse(request)
+            data = JSONParser().parse(request) # data is a dictionary
             user = User.objects.create_user(
-                username=data['username'],
-                password=data['password']
-            )
+                data['username'],
+                password=data['password'])
             user.save()
+
             token = Token.objects.create(user=user)
-            return JsonResponse({'token':str(token)},status=201)
+            return JsonResponse({'token':str(token)},status=201) 
+
         except IntegrityError:
-            return JsonResponse({'message':'User already exists'},status=400)
+            return JsonResponse({
+                'error':'username taken. choose another username'},
+                status=400)
 
 @csrf_exempt
 def login(request):
-    if request.method == 'POST':
+    if request.method == 'POST':        
         data = JSONParser().parse(request)
-        user = authenticate(request,
-        username=data['username'],
-        password=data['password'])
+        user = authenticate(
+                request,
+                username=data['username'],
+                password=data['password'])
         if user is None:
-            return JsonResponse({'message':'unable to login. check password and username'},status=400)
-        else: #return user token
+            return JsonResponse({'error':'unable to login. check username and password'},status=400)
+        else:     # return user token
             try:
-                token =  Token.objects.get(user=user)
-            except: #si el token no esta en la base de datos lo crea
+                token = Token.objects.get(user=user)
+            except: # if token not in db, create a new one
                 token = Token.objects.create(user=user)
-            return JsonResponse({'token':str(token)},status=201)
-        
+            return JsonResponse({'token':str(token)},status=201)  
